edaa5cf62177d7f4a35c8afe6f3fd24e7003be63
jkjang <jkjang81@gmail.com>
2018-05-20 07:43:02 +0900

    init

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-// const routes = require('./routes');
 
 const app = express();
 
-// app.use(routes);
-app.use(express.static(path.resolve('../../front-end/build')));
+// app.use(routes); app.get('/', render);
+app.use(express.static(path.resolve('../react-app/build')));
+// app.use(render);
 
-app.listen(8080, () => {
-    console.log('Sever is listening to port 4000!');
-})
+app.listen(4000, () => {
+    console.log('Server is listening to port 4000!');
+});
