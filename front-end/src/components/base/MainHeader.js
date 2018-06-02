635eae76718e5b620594120a050ac6ac4c30d9fd
jkjang <jkjang81@gmail.com>
2018-06-03 07:14:10 +0900

    init

--- front-end/src/components/base/MainHeader.js
+++ front-end/src/components/base/MainHeader.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './MainHeader.scss';
-import logoImage from 'process.env';
+import logoImage from 'path';
 
 const MainHeader = () => (
   <div className="main-header main-landing">
-    <div className="content-bg-wrap bg-landing"/>
+    <div className="content-bg-wrap bg-landing" />
     <div className="container">
-      <div className="row"/>]
+      <div className="row" />]
       <div className="col col-lg-6 m-auto col-md-12 col-sm-12 col-12">
-        <div className="main-header-content"/>
+        <div className="main-header-content" />
         <a href="#aa" className="logo">
           <div className="img-wrap">
-            <img src={process.env.src.} alt="Olympus"/>
+            <img src="img/logo-landing.png" alt="Olympus" />
           </div>
         </a>
       </div>
